a186d52d96de7cc47de6eee57ac4ced56e545cb6
Mark Hamstra <hello@markhamstra.com>
2019-08-15 15:39:38 +0200

    Sync translations

---
+++ core/components/commerce_pickuptime/lexicon/sv/default.inc.php
@@ -0,0 +1,6 @@
+<?php
+
+$_lang['commerce_pickuptime'] = 'PickupTime';
+$_lang['commerce_pickuptime.description'] = 'Stores the customer-selected pickup time with the order and shipments.';
+$_lang['commerce_pickuptime.pickup_at'] = 'Pick-up at [[+time]]';
+$_lang['commerce.pickuptime'] = 'Pickup Time';
